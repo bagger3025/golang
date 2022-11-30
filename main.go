aa553d4e6f60ceb5b882fcbb808bf199563d6bd5
bagger3025 <80453100+bagger3025@users.noreply.github.com>
2022-11-30 10:31:20 +0000

    1-8. Pointers!

--- main.go
+++ main.go
@@ -2,45 +2,14 @@ package main
 
 import "fmt"
 
-func canIDrink(age int) bool {
-	switch age {
-	case 10:
-		return false
-	case 18:
-		return true
-	}
-	return false
-}
-
-func canIDrink2(age int) bool {
-	switch {
-	case age < 18:
-		return false
-	case age == 18:
-		return true
-	case age > 50:
-		return false
-	default:
-		return true
-	}
-	return false
-}
-
-func canIDrink3(age int) bool {
-	switch koreanage := age + 2; {
-	case koreanage < 18:
-		return false
-	case age == 18:
-		return true
-	case age > 50:
-		return false
-	default:
-		return true
-	}
-	return false
-}
-
 func main() {
-	fmt.Println(canIDrink(20))
-	fmt.Println(canIDrink(10))
+	a := 2
+	b := &a
+	a = 5
+
+	fmt.Println(a, *b)
+
+	*b = 10
+
+	fmt.Println(a, *b)
 }
