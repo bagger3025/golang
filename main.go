3947af8ecf0eacba9045de3c1384b94409134ca9
bagger3025 <80453100+bagger3025@users.noreply.github.com>
2022-11-30 10:25:27 +0000

    1-7. Switch

--- main.go
+++ main.go
@@ -3,12 +3,43 @@ package main
 import "fmt"
 
 func canIDrink(age int) bool {
-	if koreanage := age + 2; koreanage < 18 {
+	switch age {
+	case 10:
 		return false
-	} else {
+	case 18:
 		return true
 	}
+	return false
 }
+
+func canIDrink2(age int) bool {
+	switch {
+	case age < 18:
+		return false
+	case age == 18:
+		return true
+	case age > 50:
+		return false
+	default:
+		return true
+	}
+	return false
+}
+
+func canIDrink3(age int) bool {
+	switch koreanage := age + 2; {
+	case koreanage < 18:
+		return false
+	case age == 18:
+		return true
+	case age > 50:
+		return false
+	default:
+		return true
+	}
+	return false
+}
+
 func main() {
 	fmt.Println(canIDrink(20))
 	fmt.Println(canIDrink(10))
