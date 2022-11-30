ccbd2f53dafcd2dab529ffd72ca7cdb9680dfc40
bagger3025 <80453100+bagger3025@users.noreply.github.com>
2022-11-30 10:21:08 +0000

    1-6. If with a Twist

--- main.go
+++ main.go
@@ -2,26 +2,14 @@ package main
 
 import "fmt"
 
-func superAdd2(numbers ...int) int {
-	total := 0
-	for i := 0; i < len(numbers); i++ {
-		total += numbers[i]
+func canIDrink(age int) bool {
+	if koreanage := age + 2; koreanage < 18 {
+		return false
+	} else {
+		return true
 	}
-	return total
 }
-
-func superAdd(numbers ...int) int {
-	total := 0
-	for _, number := range numbers {
-		total += number
-	}
-
-	return total
-}
-
 func main() {
-	result := superAdd(2, 3, 4, 5, 6, 7)
-	fmt.Println(result)
-	result = superAdd2(2, 3, 4, 5, 6, 7)
-	fmt.Println(result)
+	fmt.Println(canIDrink(20))
+	fmt.Println(canIDrink(10))
 }
