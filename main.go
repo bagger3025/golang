d9f4c5dc2ab91f51dd2b100473bde1468ca2d443
bagger3025 <80453100+bagger3025@users.noreply.github.com>
2022-12-01 10:46:40 +0000

    2-6. Update Delete

--- main.go
+++ main.go
@@ -21,4 +21,18 @@ func main() {
 	} else {
 		fmt.Println(dictionaries)
 	}
+
+	err3 := dictionaries.Update("hello", "Golang!")
+	if err3 != nil {
+		fmt.Println(err3)
+	} else {
+		fmt.Println(dictionaries["hello"])
+	}
+
+	err4 := dictionaries.Delete("hello")
+	if err4 != nil {
+		fmt.Println(err4)
+	} else {
+		fmt.Println("hello is deleted")
+	}
 }
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -4,8 +4,12 @@ import "errors"
 
 type Dictionary map[string]string
 
-var errNotFound = errors.New("there are no such word")
-var errWordExists = errors.New("the word already exists")
+var (
+	errNotFound   = errors.New("there are no such word")
+	errWordExists = errors.New("the word already exists")
+	errCantUpdate = errors.New("cant update non-existing word")
+	errCantDelete = errors.New("cant delete non-existing word")
+)
 
 func (d Dictionary) Search(word string) (string, error) {
 	val, ok := d[word]
@@ -31,4 +35,26 @@ func (d Dictionary) Add(word, def string) error {
 		return errWordExists
 	}
 	return nil*/
+}
+
+func (d Dictionary) Update(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = def
+	case errNotFound:
+		return errCantUpdate
+	}
+	return nil
+}
+
+func (d Dictionary) Delete(word string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		delete(d, word)
+	case errNotFound:
+		return errCantDelete
+	}
+	return nil
 }
