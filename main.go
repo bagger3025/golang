6bf75daa8f59c8dcdeef6289dc00f7d1785601f9
bagger3025 <80453100+bagger3025@users.noreply.github.com>
2022-12-01 09:21:34 +0000

    2-0. Account + NewAccount

--- main.go
+++ main.go
@@ -1,23 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type person struct {
-	name    string
-	age     int
-	favFood []string
-}
+	"github.com/bagger3025/golang/accounts"
+)
 
 func main() {
-	nico := person{"nico", 18, []string{"kimchi", "anythingelse"}}
-	nico2 := person{
-		name:    "nico",
-		age:     18,
-		favFood: []string{"kimchi", "bulgogi"},
-	}
-	fmt.Println(nico)
-	fmt.Println(nico.favFood)
-
-	fmt.Println(nico2)
-	fmt.Println(nico2.favFood)
+	account := accounts.NewAccount("nico")
+	fmt.Println(account)
 }
