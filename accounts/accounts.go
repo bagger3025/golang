6b3157cf74e6736d8915b909cb771678ed601e93
bagger3025 <80453100+bagger3025@users.noreply.github.com>
2022-12-01 09:46:46 +0000

    2-2. Methods part Two

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,19 +1,32 @@
 package accounts
 
+import "errors"
+
 type Account struct {
 	owner   string
 	balance int
 }
+
+var errNoMoney = errors.New("can't withdraw you are poor")
 
 func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
 }
 
-func (a Account) Deposit(amount int) {
+func (a *Account) Deposit(amount int) {
 	a.balance += amount
 }
 
 func (a Account) Balance() int {
 	return a.balance
+}
+
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	} else {
+		a.balance -= amount
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/bagger3025/golang/accounts"
 )
@@ -10,4 +11,9 @@ func main() {
 	account := accounts.NewAccount("nico")
 	account.Deposit(10)
 	fmt.Println(account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("err was", err, "balance is", account.Balance())
 }
