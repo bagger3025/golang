ae4db5c33dec425285731ec4fae82df4ada36abf
bagger3025 <80453100+bagger3025@users.noreply.github.com>
2022-12-01 10:38:40 +0000

    2-5. Add Method

--- main.go
+++ main.go
@@ -14,4 +14,11 @@ func main() {
 	} else {
 		fmt.Println(val)
 	}
+
+	err2 := dictionaries.Add("hello", "world")
+	if err2 != nil {
+		fmt.Println(err2)
+	} else {
+		fmt.Println(dictionaries)
+	}
 }
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -5,6 +5,7 @@ import "errors"
 type Dictionary map[string]string
 
 var errNotFound = errors.New("there are no such word")
+var errWordExists = errors.New("the word already exists")
 
 func (d Dictionary) Search(word string) (string, error) {
 	val, ok := d[word]
@@ -13,4 +14,21 @@ func (d Dictionary) Search(word string) (string, error) {
 	} else {
 		return "", errNotFound
 	}
+}
+
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+	return nil
+	/*if err == errNotFound {
+		d[word] = def
+	} else if err == nil {
+		return errWordExists
+	}
+	return nil*/
 }
