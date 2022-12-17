3ec043246fa6ff176b68aba02983af84cca75107
bagger3025 <80453100+bagger3025@users.noreply.github.com>
2022-12-17 08:50:31 +0000

    3-1. Slow URLChecker

--- urlChecker/urlChecker.go
+++ urlChecker/urlChecker.go
@@ -7,7 +7,7 @@ import (
 )
 
 var (
-	errRequestFailed = errors.New("Request failed")
+	errRequestFailed = errors.New("request failed")
 )
 
 func UrlCheckerMain() {
@@ -16,15 +16,29 @@ func UrlCheckerMain() {
 		"https://www.google.com",
 		"https://soundcloud.com",
 		"https://www.airbnb.com",
-		"https://www.redit.com",
+		"https://www.reddit.com",
 		"https://www.amazon.com",
-		"https://academy.nomadcoders.io",
+		"https://academy.nomadcoders.co",
 		"https://www.github.com",
 		"https://www.notion.so",
 	}
 
+	results := make(map[string]string)
+
 	for _, url := range urls {
-		hitURL(url)
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+
+		results[url] = result
+	}
+
+	// fmt.Println(results)
+
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
 
@@ -32,6 +46,8 @@ func hitURL(url string) error {
 	fmt.Println("Checking:", url)
 	resp, err := http.Get(url)
 	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err)
+		fmt.Println(resp.StatusCode)
 		return errRequestFailed
 	}
 
